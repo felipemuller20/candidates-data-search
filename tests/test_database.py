f2b70197283b8a13e9122bdf08d8994fa3a876ca
Felipe Muller <felipeavmuller@gmail.com>
2022-01-26 23:02:53 -0300

    feat: all database functions tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,15 +3,20 @@ from candidates_data_search.database import (
     create_table,
     add_candidate,
     get_candidate,
+    remove_candidate,
 )
 from candidates_data_search.Candidate import Candidate
 
 
-def test_add_candidate():
+def test_database():
     create_database()
     create_table()
 
     candidate = Candidate('TEST NAME 12399', '99999999999', '99.99')
     add_candidate(candidate)
-    candidate_exists = get_candidate("99999999999")
+    candidate_exists = get_candidate(candidate.cpf)
     assert candidate_exists is not None
+
+    remove_candidate(candidate.cpf)
+    candidate_exists = get_candidate(candidate.cpf)
+    assert candidate_exists is None
